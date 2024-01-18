e2e010d97d1545dcfa6b68112eff65108527305d
Shajahan <36882327+arshajahan@users.noreply.github.com>
2024-01-18 09:58:32 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
               </>
           } />
           
-          <Route path='/' 
+          <Route path='/home' 
             element={
               <>
                 <Banner/>
