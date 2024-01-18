164b91e926aabde41d5ca0c4edc7d3dd779dc909
Shajahan <shajahan.ar09@gmail.com>
2024-01-18 19:41:24 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <Navbar/>
         <Routes>
           
-          <Route path='/home' 
+          <Route path='/' 
             element={
               <>
                 <Banner/>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/home', // Update with your repository name
+  base: '', // Update with your repository name
 });
