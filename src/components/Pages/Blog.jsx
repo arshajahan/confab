79d64f9d8543aa2708ebb6a21294219dfb3e6f48
Shajahan <shajahan.ar09@gmail.com>
2024-02-20 21:57:12 +0400

    og tags added

--- src/components/Pages/Blog.jsx
+++ src/components/Pages/Blog.jsx
@@ -18,26 +18,28 @@ function Blog() {
     return (
         <>
             <Helmet>
-                <title>Confab International Blog</title>
-                <meta 
-                    name='description' 
-                    content={selectedBlog.title}
-                />
-                <link rel='canonical' href={`/blog/${selectedBlog.path}`}/>
+                <title>{selectedBlog.title}</title>
+                <meta name='description' content={selectedBlog.title} />
+                <meta property='og:title' content={selectedBlog.title} />
+                <meta property='og:description' content={selectedBlog.title} />
+                <meta property='og:image' content={selectedBlog.src} />
+                <meta property='og:url' content={`https://confabint.com/blog/${selectedBlog.path}`} />
+                <meta property='og:type' content='article' />
             </Helmet>
             <div>
                 <div className='relative h-[600px] lg:h-screen flex items-center'>
                     <img
-                    title={selectedBlog.title}
-                    className='absolute top-0 left-0 w-full h-full object-cover z-0'
-                    src={selectedBlog.src}
-                    alt={selectedBlog.path}
-                    height={100} width={100}
+                        title={selectedBlog.title}
+                        className='absolute top-0 left-0 w-full h-full object-cover z-0'
+                        src={selectedBlog.src}
+                        alt={selectedBlog.path}
+                        height={100}
+                        width={100}
                     />
                 </div>
                 <WrapperCard className='text-main'>
                     <div className='py-12 lg:px-48'>
-                        <h1 className=' text-2xl lg:text-4xl'>{selectedBlog.title}</h1><br/>
+                        <h1 className=' text-2xl lg:text-4xl'>{selectedBlog.title}</h1><br />
                         <div className='flex justify-between items-center mt-4'>
                             <p className=''>{selectedBlog.date}</p>
                             <div>
@@ -45,8 +47,8 @@ function Blog() {
                                 <button onClick={shareViaWhatsApp} className='bg-green-500 text-white text-xs px-3 py-2 rounded-md'>Share via WhatsApp</button>
                             </div>
                         </div>
-                        <br/>
-                        { selectedBlog.content}
+                        <br />
+                        {selectedBlog.content}
                     </div>
                 </WrapperCard>
             </div>
